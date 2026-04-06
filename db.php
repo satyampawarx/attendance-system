d7324473a2b395754ee255c564e336399078d918
Satyam Pawar <pawarsatyam25@gmail.com>
2026-04-06 21:01:39 +0530

    Database connected deployed

--- db.php
+++ db.php
@@ -1,5 +1,12 @@
 <?php
-$conn = pg_connect("host=localhost dbname=attendance user=postgres password=Pass@123");
+$conn = pg_connect("
+host=ep-wandering-sea-a16rzjhf-pooler.ap-southeast-1.aws.neon.tech
+port=5432
+dbname=attendance
+user=neondb_owner
+password=npg_aM9uDmfJd3Fg
+sslmode=require
+");
 
 if(!$conn){
     echo "Database connection failed";
